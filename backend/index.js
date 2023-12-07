d77749cee91c6a0e1be777e8b73334c84335aaff
ritwanxidig <ritwanrashit@gmail.com>
2023-12-07 20:39:15 +0300

    added: some files

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB_URL)
+  .then((res) => {
+    console.log("connected");
+  })
+  .catch((er) => {
+    console.log(er);
+  });
 
 const app = express();
 
