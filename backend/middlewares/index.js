0185db59f7e8e8fac2daaf6d2c5584379d54949d
ritwanxidig <ritwanrashit@gmail.com>
2023-12-10 11:16:07 +0300

    added: isAuthenticated middleware

--- backend/middlewares/index.js
+++ backend/middlewares/index.js
@@ -1,5 +1,20 @@
+import { errorHandler } from "../Utils/error.js";
+import jwt from "jsonwebtoken";
+
 export const ExceptionHandlerMiddleware = (error, req, res, next) => {
   const statusCode = error.statusCode || 500;
   const message = error.message || "Internal Server Error";
   return res.status(statusCode).json({ success: false, statusCode, message });
+};
+
+export const IsAuthenticated = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) next(errorHandler(401, "UnAuthorized users"));
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(errorHandler(403, "Forbidden"));
+
+    req.user = user;
+    next();
+  });
 };
